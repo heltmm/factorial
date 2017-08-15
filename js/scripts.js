6ccd22a85591cffe02f5092e068ea11cd4b55421
Mark Helt and rebecca mcintosh <mark@>
2017-08-15 15:42:15 -0700

    add palindrone checker

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
   $("#userInput").submit(function(event){
+    event.preventDefault();
     var number = $("#enteredNumber").val();
     var total = number
     for(i=number-1; i > 0; i= i-1){
       total = total * i;
     }
     alert(total);
+  });
+  $("#palindroneForm").submit(function(event){
+    event.preventDefault();
+    var string = $("#words").val();
+
+    var test = string.split("").reverse().join("");
+    if (test === string){
+      alert("Palindrone")
+    }else{
+      alert("Not palindrone")
+    }
+
   });
 });
